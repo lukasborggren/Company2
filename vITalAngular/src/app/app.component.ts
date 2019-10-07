2b71d5fa757a4144ed9a0a5471beee0ca08c92a2
David Van Gheel <davva679@student.liu.se>
2019-10-07 14:36:15 +0200

    [U14 T2][David Van Gheel][Add quagga as injectable to scan barcode][V 1.0]

--- vITalAngular/src/app/app.component.ts
+++ vITalAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {BarcodeScannerService} from './barcode-scanner.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'vITalAngular';
+
+  constructor(
+      private barcodeScanner: BarcodeScannerService,
+  ) { }
+
+  startScanner() {
+    this.barcodeScanner.startScanner();
+  }
 }
