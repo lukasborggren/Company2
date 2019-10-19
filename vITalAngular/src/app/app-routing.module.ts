b67ffa5360bc88dd83951868f9e1b7f882b6b739
Rasmus Lindblom <rallei.lindblom@gmail.com>
2019-10-19 11:46:20 +0200

    [U1 T6] [Added an auth.guard, active on /home]

--- vITalAngular/src/app/app-routing.module.ts
+++ vITalAngular/src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Router, RouterModule, Routes} from '@angular/router';
+import {Router, RouterModule, Routes, CanActivate} from '@angular/router';
 import { BarcodeScannerPageComponent } from './barcode-scanner-page/barcode-scanner-page.component';
 import { ManualInputDialogComponent } from './manual-input-dialog/manual-input-dialog.component';
 import {MatDialogModule} from '@angular/material';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {LogoutComponent} from './logout/logout.component';
+import { AuthGuardService as AuthGuard } from './auth-guard.service';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
-  {path: 'scannerpage', component: BarcodeScannerPageComponent},
+  {path: 'scannerpage', component: BarcodeScannerPageComponent },
   { path: '**', redirectTo: 'login' },
   {path: '', redirectTo: 'scannerpage', pathMatch: 'full'}
 
