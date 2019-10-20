272552192a7cdbd175b3c352361c87eee73a216b
Rasmus Lindblom <rallei.lindblom@gmail.com>
2019-10-20 12:02:05 +0200

    [U1 T6] [FIxed unit test for authguard]

--- vITalAngular/src/app/auth-guard.service.spec.ts
+++ vITalAngular/src/app/auth-guard.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AuthGuardService } from './auth-guard.service';
+import { RouterTestingModule } from '@angular/router/testing';
+
 
 describe('AuthGuardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule,  RouterTestingModule.withRoutes([])],
+    providers: [ AuthGuardService ]
+  }));
 
   it('should be created', () => {
     const service: AuthGuardService = TestBed.get(AuthGuardService);
--- vITalAngular/src/app/auth-guard.service.ts
+++ vITalAngular/src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router, CanActivate, RouterModule } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -7,7 +7,7 @@ import { AuthService } from './auth.service';
 })
 export class AuthGuardService implements CanActivate{
 
-  constructor(public auth: AuthService, public router: Router) { }
+  constructor(public auth: AuthService, private router: Router) { }
 
   canActivate(): boolean {
       if (!this.auth.isLoggedIn()) {
