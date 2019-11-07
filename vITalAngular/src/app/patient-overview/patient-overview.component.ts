f513eb33131f2c208ea66a493565c3240bd86f83
David Van Gheel <davva679@student.liu.se>
2019-11-07 11:32:49 +0100

    [U16&U17-T1]Get the specified vital data from the json response

--- vITalAngular/src/app/patient-overview/patient-overview.component.ts
+++ vITalAngular/src/app/patient-overview/patient-overview.component.ts
@@ -9,6 +9,18 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class PatientOverviewComponent implements OnInit {
   patientinfo: string;
+  personnumber: string;
+  info: string;
+  respiratoryRate: number;
+  oxygenSaturation: number;
+  systolicBloodPressure: number;
+  systolicBloodPressureUnit: string;
+  pulseRate: number;
+  temperature: number;
+  temperatureUnit: string;
+  supplementalOxygen: boolean;
+  consciousness: string;
+
   constructor(
       private patientService: PatientService,
       private route: ActivatedRoute
@@ -16,8 +28,20 @@ export class PatientOverviewComponent implements OnInit {
 
   ngOnInit() {
     const pid = this.route.snapshot.paramMap.get('personid');
-    console.log('pid = ' + pid);
-    this.patientService.getPatientDataPid(pid).subscribe(info => this.patientinfo = JSON.stringify(info));
+    this.patientService.getPatientDataPid(pid).subscribe(info => {
+      this.patientinfo = JSON.stringify(info);
+      this.personnumber = info['demographics']['additionalInfo']['Personnummer'];
+      this.respiratoryRate = info['breathing_frequency'];
+      this.systolicBloodPressure = (info['vital_signs']['blood_pressure'][0]['any_event'][0])['systolic'][0]['|magnitude'];
+      this.systolicBloodPressureUnit = (info['vital_signs']['blood_pressure'][0]['any_event'][0]['systolic'][0]['|unit']);
+      this.oxygenSaturation = info['oxygen_saturation'];
+      this.pulseRate = info['pulse'];
+      this.temperature = info['vital_signs']['body_temperature'][0]['any_event'][0]['temperature'][0]['|magnitude'];
+      this.temperatureUnit = info['vital_signs']['body_temperature'][0]['any_event'][0]['temperature'][0]['|unit'];
+      //TODO: implement an actual check
+      this.supplementalOxygen = false;
+      this.consciousness = info['alertness'];
+    });
   }
 
 }
